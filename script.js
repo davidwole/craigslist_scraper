311cd7a32c3f5bdbf19253f1040400df60a0594d
davidwole <davidwole20@gmail.com>
2024-07-23 04:55:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,18 @@ const transporter = nodemailer.createTransport({
 });
 
 async function scrapeData(url) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
     try{
 
       const page = await browser.newPage();

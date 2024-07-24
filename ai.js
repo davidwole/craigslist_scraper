1f60b8ff0081c2fd603bef4f6cddc9713a207c1f
David Oluwole <david@Davids-MacBook-Air.local>
2024-07-24 20:58:43 +0100

    removed webdata

--- ai.js
+++ ai.js
@@ -5,8 +5,6 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-const { webdata } = require('./obj');
- 
 
 async function checkPostRelated(body) {
   const prompt = `Does the following post state that it is looking for a freelance web designer, web developer, graphic designer, or 3D artist? If yes, reply "true"; if not, reply "false": ${body}`;

476292db46c9c93846866bcf6e05ddfc54a83741
David Oluwole <david@Davids-MacBook-Air.local>
2024-07-24 20:59:38 +0100

    set interval

--- server.js
+++ server.js
@@ -11,16 +11,12 @@ const PORT = process.env.PORT || 3000;
 function handleRootRequest (req, res) {
     console.log('Request received at /');
     res.writeHead(200, { 'Content-Type': 'text/plain' });    
-    // setInterval(function() {
-    //   scrapeData(urls[0]);
-    //   scrapeData(urls[1]);
-    //   scrapeData(urls[2]);
-    //   scrapeData(urls[3]);
-    // }, 7 * 60 * 1000);
-    scrapeData(urls[0]);
+    setInterval(function() {
+      scrapeData(urls[0]);
       scrapeData(urls[1]);
       scrapeData(urls[2]);
       scrapeData(urls[3]);
+    }, 5 * 60 * 1000);
     res.end('Scraper started');
 }
 

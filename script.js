1f059785f4b6b547f67d881f466fc121c1411524
davidwole <davidwole20@gmail.com>
2024-07-27 05:41:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ try{
       const postedTime = new Date(result.posted);
       const timeDifference = (currentTime - postedTime) / (1000 * 60); // Difference in minutes
 
-      return timeDifference < 60 * .12; 
+      return timeDifference < 60 * .10; 
   });
   
   let bodyFilteredResults = [];
